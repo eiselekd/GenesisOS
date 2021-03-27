76f6e3875265d2b27ef5f39b8b0a3a2939797665
Th3T3chn0G1t <prs.ttg+gh@pm.me>
2021-03-27 22:44:30 +0000

    Missed some unstaged changes :3

--- src/kernel/boot/include/boot.h
+++ src/kernel/boot/include/boot.h
@@ -5,7 +5,7 @@
 
 #include "../../../libc/include/stdint.h"
 #include "../../../libc/include/stddef.h"
-#include "stivale2.h"
+#include "../../../vendor/stivale/stivale2.h"
 #include "../../driver/include/terminal.h"
 
 void *stivale2_get_tag(struct stivale2_struct *stivale2_struct, uint64_t id);
